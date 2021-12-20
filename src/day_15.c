403426f35066039ed17e4cbb5818f3e98631d672
Thomas Plews <tplews@cisco.com>
2021-12-20 11:11:21 +0000

    Add day 15 input

---
+++ src/day_15.c
@@ -0,0 +1,52 @@
+/*
+ * AoC 2021 Day 15 solution
+ */
+
+#include "utils.h"
+
+/*
+ * runner
+ *
+ * Function which performs the days task. Repeat multiple times to benchmark.
+ *
+ * Argument: file_name
+ *     File to read input from.
+ * Argument: print_output
+ *     Whether to print output or not. This should only be done on the final
+ *     run to not spam the console.
+ *
+ * Return: void
+ */
+static void
+runner(char *file_name, bool print_output)
+{
+    parsed_text_type parsed_text;
+
+    parsed_text = parse_file(file_name);
+
+    if (print_output) {
+        printf("Part 1:\n");
+    }
+
+    if (print_output) {
+        printf("Part 2:\n");
+    }
+
+    free_parsed_text(parsed_text);
+}
+
+/*
+ * Main function.
+ */
+int
+main(int argc, char **argv)
+{
+    char *file_name = NULL;
+
+    assert(argc == 2);
+    file_name = argv[1];
+
+    run_main_func_with_benchmark(runner, file_name);
+
+    return (0);
+}
